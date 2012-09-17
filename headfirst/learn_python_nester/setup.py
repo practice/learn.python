ac73ce7433ef0b46a180cc8587fee33b06558f13
shawn.won <shawn.won@gmail.com>
2012-09-17 13:51:22 +0900

    create learn_python_nester module

---
+++ headfirst/learn_python_nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'nester',
+	version = '1.0.0',
+	py_modules = ['learn_python_nester'],
+	author = 'shawn.won',
+	author_email = 'shawn.won@gmail.com',
+	url = 'http://blog.jabberstory.net',
+	description = 'A simple printer of nested lists',
+)
